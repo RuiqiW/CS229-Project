b58cf82cd47c5abbca0bb2cb0e6689e16665bff7
RuiqiW <ruiqi134@gmail.com>
2023-05-27 00:22:22 -0700

    updated models

--- src/model.py
+++ src/model.py
@@ -62,17 +62,17 @@ class VanillaPointNet(torch.nn.Module):
 class VoxelCNN(nn.Module):
     def __init__(self, num_classes):
         super(VoxelCNN, self).__init__()
-        self.conv1 = nn.Conv3d(1, 2, kernel_size=3, padding=1)
-        # self.fc1 = nn.Linear(576, 128)
-        self.fc2 = nn.Linear(72, num_classes)
+        self.conv1 = nn.Conv3d(1, 16, kernel_size=3, padding=1)
+        self.fc1 = nn.Linear(576, 128)
+        self.fc2 = nn.Linear(128, num_classes)
 
     def forward(self, x):
         x = x.unsqueeze(dim=1)
         x = F.relu(self.conv1(x))
         x = F.max_pool3d(x, kernel_size=(6, 6, 8)) # Cx6x6x1
-        x = x.view(-1, 72)
+        x = x.view(-1, 576)
 
-        # x = F.relu(self.fc1(x))
+        x = F.relu(self.fc1(x))
         x = self.fc2(x)
         return F.log_softmax(x, dim=1)
     
--- src/train.py
+++ src/train.py
@@ -11,17 +11,19 @@ from dataset import ImageDataset, PointCloudDataset, VoxelDataset
 from model import MLP, CNN, VanillaPointNet, VoxelCNN
 from pointnet import PointNetMini
 
+torch.manual_seed(1234)
+
 
 DATA_FORMATS = ['single_view','pcd', 'voxel']
 
 PREFIX = 'train'
 
-DATA_FORMAT = 'pcd'
+DATA_FORMAT = 'voxel'
 ROOT_DIR = '../data/train_{}'.format(DATA_FORMAT)
 DATA_LABELS = '../data/train_meshMNIST/labels.txt'
 
-BATCH_SIZE = 32
-EPOCHS = 20
+BATCH_SIZE = 128
+EPOCHS = 30
 
 transform = transforms.Compose([
     transforms.ToTensor()
@@ -67,8 +69,8 @@ if __name__ == '__main__':
         val_dataset = PointCloudDataset(ROOT_DIR, val_lines, filename_format=filename_format)
         test_dataset = PointCloudDataset(ROOT_DIR, test_lines, filename_format=filename_format)
 
-        # model = VanillaPointNet(num_classes=10)
-        model = PointNetMini(num_classes=10)
+        model = VanillaPointNet(num_classes=10)
+        # model = PointNetMini(num_classes=10)
 
     elif DATA_FORMAT == 'voxel':
         filename_format = "{:05d}.npy"
@@ -80,13 +82,14 @@ if __name__ == '__main__':
     else:
         raise NotImplementedError('Data format not supported')
 
-
+    device = "cuda"
+    model = model.to(device)
     train_loader = DataLoader(train_dataset, batch_size=BATCH_SIZE, shuffle=True)
     val_loader = DataLoader(val_dataset, batch_size=BATCH_SIZE, shuffle=False)
     test_loader = DataLoader(test_dataset, batch_size=BATCH_SIZE, shuffle=False)
 
     
-    optimizer = optim.Adam(model.parameters(), lr=0.01)
+    optimizer = optim.Adam(model.parameters(), lr=0.003)
     criterion = nn.CrossEntropyLoss()
 
     print("data format: ", DATA_FORMAT)
@@ -100,6 +103,8 @@ if __name__ == '__main__':
         correct = 0
 
         for batch_idx, (data, target) in enumerate(tqdm(train_loader)):
+            data = data.to(device)
+            target =  target.to(device)
             optimizer.zero_grad()
             output = model(data)
             loss = criterion(output, target)
@@ -119,6 +124,8 @@ if __name__ == '__main__':
         correct = 0
         with torch.no_grad():
             for batch_idx, (data, target) in enumerate(tqdm(val_loader)):
+                data = data.to(device)
+                target = target.to(device)
                 output = model(data)
                 val_loss += criterion(output, target)
                 pred = output.argmax(dim=1, keepdim=True)
@@ -131,17 +138,19 @@ if __name__ == '__main__':
             
 
 
-    model.eval()
-    test_loss = 0
-    correct = 0
-    with torch.no_grad():
-        for batch_idx, (data, target) in enumerate(tqdm(test_loader)):
-            output = model(data)
-            test_loss += criterion(output, target)
-            pred = output.argmax(dim=1, keepdim=True)
-            correct += pred.eq(target.view_as(pred)).sum().item()
+        model.eval()
+        test_loss = 0
+        correct = 0
+        with torch.no_grad():
+            for batch_idx, (data, target) in enumerate(tqdm(test_loader)):
+                data = data.to(device)
+                target = target.to(device)
+                output = model(data)
+                test_loss += criterion(output, target)
+                pred = output.argmax(dim=1, keepdim=True)
+                correct += pred.eq(target.view_as(pred)).sum().item()
 
-    test_loss /= len(test_loader.dataset)
-    accuracy = 100. * correct / len(test_loader.dataset)
-    print('Test set: Average loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)'.format(test_loss, correct, 
-                                                                             len(test_loader.dataset), accuracy))
+        test_loss /= len(test_loader.dataset)
+        accuracy = 100. * correct / len(test_loader.dataset)
+        print('Test set: Average loss: {:.4f}, Accuracy: {}/{} ({:.2f}%)'.format(test_loss, correct, 
+                                                                                len(test_loader.dataset), accuracy))
